9104c4797b48a9a8f2f063efc35106733924cbbe
Andre Lammel <andre.lammel@theneglectedmachine.org>
2018-09-08 20:13:24 +0200

    Enable Log encrpytion

--- resources_s3_bucket_logging.tf
+++ resources_s3_bucket_logging.tf
@@ -23,6 +23,14 @@ resource "aws_s3_bucket" "logging-bucket" {
     }
   }
 
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "aws:kms"
+      }
+    }
+  }
+
   tags = "${merge(
     local.tags,
     map(
